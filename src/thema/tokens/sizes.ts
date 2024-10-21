a9cc16220546049b87fa12a6b7b9f6ca7a78051d
Mehmettakkan <mehmetakkan18@gmail.com>
2024-10-21 15:06:43 +0300

    Update sizes.ts for responsive design
    - Adjusted the size definitions for containers to ensure clarity and consistency.

--- src/thema/tokens/sizes.ts
+++ src/thema/tokens/sizes.ts
@@ -1,4 +1,22 @@
-// src/theme/tokens/sizes.ts
+// // src/theme/tokens/sizes.ts
+// import { defineTokens } from '@pandacss/dev'
+
+// export const sizes = defineTokens.sizes({
+//   container: {
+//     xs: { value: "360px" },    // Mobil telefonlar için
+//     sm: { value: "576px" },    // Büyük mobil telefonlar ve küçük tabletler için
+//     md: { value: "768px" },    // Tabletler için
+//     lg: { value: "992px" },    // Küçük dizüstü bilgisayarlar için
+//     xl: { value: "1200px" },   // Orta boy ekranlar için
+//     "2xl": { value: "1440px" },// Büyük ekranlar için
+//     "3xl": { value: "1600px" },// Daha büyük ekranlar için
+//     "4xl": { value: "1920px" },// Full HD ekranlar için
+//     "5xl": { value: "2560px" },// 2K ekranlar için
+//     "6xl": { value: "3200px" },// Ultra geniş 4K ekranlar için
+//   },
+// })
+
+
 import { defineTokens } from '@pandacss/dev'
 
 export const sizes = defineTokens.sizes({
@@ -6,12 +24,12 @@ export const sizes = defineTokens.sizes({
     xs: { value: "360px" },    // Mobil telefonlar için
     sm: { value: "576px" },    // Büyük mobil telefonlar ve küçük tabletler için
     md: { value: "768px" },    // Tabletler için
-    lg: { value: "992px" },    // Küçük dizüstü bilgisayarlar için
-    xl: { value: "1200px" },   // Orta boy ekranlar için
-    "2xl": { value: "1440px" },// Büyük ekranlar için
-    "3xl": { value: "1600px" },// Daha büyük ekranlar için
-    "4xl": { value: "1920px" },// Full HD ekranlar için
-    "5xl": { value: "2560px" },// 2K ekranlar için
+    lg: { value: "1200px" },   // Küçük dizüstü bilgisayarlar için
+    xl: { value: "1440px" },   // Orta boy ekranlar için
+    "2xl": { value: "1600px" },// Büyük ekranlar için
+    "3xl": { value: "1920px" },// Daha büyük ekranlar için
+    "4xl": { value: "2560px" },// Full HD ekranlar için
+    "5xl": { value: "3200px" },// 2K ekranlar için
     "6xl": { value: "3200px" },// Ultra geniş 4K ekranlar için
   },
 })
